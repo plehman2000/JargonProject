ee23fcca158d16783c2aa3d15f768365cd21c0ca
Jordan Sabo <sabo.jordan121@gmail.com>
2022-03-26 18:05:20 -0400

    cleaning up environment, now using anaconda

--- GUI/app.py
+++ GUI/app.py
@@ -7,11 +7,14 @@ import pdfplumber
 app = Flask(__name__, template_folder='templates')
 
 app.config["UPLOAD_FOLDER"] = "static/"
+app.config["FLASK_APP"] = "app.py"
 
+#initial page to upload pdf
 @app.route("/")
 def upload_file():
     return render_template('index.html')
 
+#displays the uploaded pdf
 @app.route("/display", methods = ['GET', 'POST'])
 def display_file():
     if request.method == 'POST':
@@ -25,9 +28,17 @@ def display_file():
         while page < len(pdf.pages):
             content+="\n" + pdf.pages[page].extract_text()
             page+=1
-        
+        pdf.close()
     return render_template('content.html', content=content) 
 
+#clears out current static folder
+@app.route("/delete")
+def delete_files():
+    dir = "static/"
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+    return render_template('index.html')
+    
 if(__name__) == "__main__":
     app.run()
 
